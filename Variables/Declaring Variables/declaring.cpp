3998ee66dd71d01509faae37a4730de21c4ad5d1
cwalker <gatiulo@gmail.com>
2023-01-30 22:02:46 -0600

    Update Variables

--- Variables/Declaring Variables/declaring.cpp
+++ Variables/Declaring Variables/declaring.cpp
@@ -1,12 +1,12 @@
 
 int main()
 {
-    int integerNumber = 5;
-    int multipleDigitInteger = 10005;
-    double floatingPointNumber = 5.7;
-    bool positiveBoolean = true;
-    bool negativeBoolean = false;
-    char letterG = 'G';
-    long long superBig = 50000000000;
-    short shorterNumber = 24;
+    int integerNumber;
+    int multipleDigitInteger;
+    double floatingPointNumber;
+    bool positiveBoolean;
+    bool negativeBoolean;
+    char letterG;
+    long long superBig;
+    short shorterNumber;
 }
---
+++ Variables/Literal and Variable/literals.cpp
@@ -0,0 +1,5 @@
+// Here the variable is the integer
+// with the name number.
+// The number 5 being assigned to this
+// variable is a literal.
+int number = 5;
